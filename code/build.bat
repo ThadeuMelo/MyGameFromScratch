1de5450783d42c4376ce9a5ad60e63b69524e4dd
ThadeuFerreira <thadeu_afmelo@hotmail.com>
2014-12-01 10:27:50 -0200

    .

--- code/build.bat
+++ code/build.bat
@@ -1,5 +1,5 @@
 @echo off
-mkdir ..\build
-pushd ..\build
+mkdir ..\..\build
+pushd ..\..\build
 cl -Zi c:\handmadehero\HandMadeHero\code\main_handmadehero.cpp user32.lib Gdi32.lib
 popd
--- code/debug.bat
+++ code/debug.bat
@@ -1,5 +1,5 @@
 @echo off
-mkdir ..\build
-pushd ..\build
+mkdir ..\..\build
+pushd ..\..\build
 devenv main_handmadehero.exe
 popd
