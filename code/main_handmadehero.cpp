c8f280600f3c5666f75109323989e3f301819139
ThadeuFerreira <thadeu_afmelo@hotmail.com>
2014-12-01 22:40:57 -0200

    Dia 3
    Criando o BitMap na Janela

--- code/main_handmadehero.cpp
+++ code/main_handmadehero.cpp
@@ -2,7 +2,16 @@
 
 #include <windows.h>
 
-static bool Running;
+#define local_persist static
+#define global_variable static
+#define internal static
+
+global_variable bool Running;
+global_variable BITMAPINFO BitMapInfo;
+global_variable void *BitMapMemory;
+global_variable HBITMAP BitMapHandle;
+global_variable HDC BitMapDeviceContext;
+
 LRESULT CALLBACK Win32MainWindowCallBack(
 	HWND	Window,
 	UINT	Message,
@@ -11,6 +20,55 @@ LRESULT CALLBACK Win32MainWindowCallBack(
 	);
 	
 LRESULT Win32CreateInitialWindow(HINSTANCE Instance);
+
+internal void Win32ResizeDIBSection(int, int);
+
+internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Hight);
+
+internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Hight)
+{
+	StretchDIBits(	DeviceContext,
+		X, Y, Width, Hight,
+		X, Y, Width, Hight,
+		BitMapMemory,
+		&BitMapInfo,
+		DIB_RGB_COLORS,
+		SRCCOPY
+		);
+
+}
+
+internal void Win32ResizeDIBSection(int Width, int Hight)
+{
+	//TODO(thadeu): Free our DIB Section
+
+	if (BitMapHandle)
+	{
+		DeleteObject(BitMapHandle);
+	}
+	if (!BitMapDeviceContext)
+	{
+		BitMapDeviceContext = CreateCompatibleDC(0);
+	}
+
+	BitMapInfo.bmiHeader.biSize = sizeof(BitMapInfo.bmiHeader);
+	BitMapInfo.bmiHeader.biHeight = Hight;
+	BitMapInfo.bmiHeader.biWidth = Width;
+	BitMapInfo.bmiHeader.biPlanes = 1;
+	BitMapInfo.bmiHeader.biBitCount = 32;
+	BitMapInfo.bmiHeader.biCompression = BI_RGB;
+
+
+	BitMapHandle = CreateDIBSection(BitMapDeviceContext,
+									&BitMapInfo,
+									DIB_RGB_COLORS,
+									&BitMapMemory,
+									0,
+									0);
+
+}
+
+
 
 LRESULT Win32CreateInitialWindow(HINSTANCE Instance){
 	WNDCLASS WindowClass = {};
@@ -40,6 +98,7 @@ LRESULT Win32CreateInitialWindow(HINSTANCE Instance){
 		if (WindowHandler)
 		{
 			MSG Message;
+			Running = true;
 			while (Running){
 				BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
 				if (MessageResult > 0)
@@ -86,8 +145,8 @@ LRESULT CALLBACK Win32MainWindowCallBack(
 			int Y = Paint.rcPaint.top;
 			int Hight = Paint.rcPaint.bottom - Paint.rcPaint.top;
 			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-
-			PatBlt(DeviceContext, X, Y, Width, Hight, WHITENESS);
+			
+			Win32UpdateWindow(DeviceContext, X, Y, Width, Hight);
 			EndPaint(Window, &Paint);
 		} break;
 		case WM_SIZE:
@@ -96,7 +155,7 @@ LRESULT CALLBACK Win32MainWindowCallBack(
 			GetClientRect(Window, &ClientRect);
 			int Width = ClientRect.right - ClientRect.left;
 			int Hight = ClientRect.bottom - ClientRect.top;
-			//ResizeDIBSection(Width, Hight);
+			Win32ResizeDIBSection(Width, Hight);
 			OutputDebugStringA("WM_SIZE\n");
 		} break;
 		case WM_DESTROY:
@@ -132,7 +191,6 @@ int CALLBACK WinMain(
 	int ShowCode
 	)
 {
-	Running = true;
 	Win32CreateInitialWindow(Instance);
 
 	return 0;
